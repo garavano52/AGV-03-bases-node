d52b6f88614de4f0e362bb387fb956c954645282
Alejandro Garavano Viotti <garavano52@gmail.com>
2022-04-17 04:39:08 -0300

    Readme agregado

---
+++ comandosGit.js
@@ -0,0 +1,4 @@
+// git init                             => Inicializo git
+// git add .                            => Agrego a la lista para hacer commit de todos los cambios
+// git commit -m "Primer Commit"        => Realizo el commit
+// git checkout -- .                    => Restauro el mismo
