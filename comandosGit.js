8883477d14abf756ada5acfb993e4f4adb6c9e98
Alejandro Garavano Viotti <garavano52@gmail.com>
2022-04-17 04:47:27 -0300

    Archivo Comando Git

--- comandosGit.js
+++ comandosGit.js
@@ -2,3 +2,17 @@
 // git add .                            => Agrego a la lista para hacer commit de todos los cambios
 // git commit -m "Primer Commit"        => Realizo el commit
 // git checkout -- .                    => Restauro el mismo
+
+// git remote add origin https://github.com/garavano52/AGV-03-bases-node.git        => Para subir por 1° vez
+// git branch -M main                                                               => Para subir por 1° vez
+// git push -u origin main                                                          => Para subir por 1° vez
+
+// git push                                                                         => Para subir otra vez
+
+// Subir por 2da vez
+// git add .
+// git commit -m "Segundo Commit"
+// git push 
+
+// Si se baja del repositorio y se quiere usar. Debemos ejecutar el comdando:
+// npm install  => Para que saque del package.json los paquetes necesarios y los instale.
